3017aa3fc05851274506e4df2bdb87c71400f157
Rubens Nascimento <rubens@allinvestx.com>
2019-11-27 15:47:26 -0300

    fix

--- src/views/Occupation/OccupationInfo.vue
+++ src/views/Occupation/OccupationInfo.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="occupation-video">
       <iframe
-        src="//www.youtube.com/embed/tJp2yKYSz5c?wmode=transparent&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;rel=0"
+        src="//www.youtube.com/embed/nLPssMXOEx"
         width="100%"
         height="100%"
         frameborder="0"
@@ -13,21 +13,10 @@
     </div>
 
     <p>
-      On no twenty spring of in esteem spirit likely estate. Continue new you
-      declared differed learning bringing honoured. At mean mind so upon they
-      rent am walk. Shortly am waiting inhabit smiling he chiefly of in. Lain
-      tore time gone him his dear sure. Fat decisively estimating affronting
-      assistance not. Resolve pursuit regular so calling me. West he plan girl
-      been my then up no.
-    </p>
-    <p>
-      New had happen unable uneasy. Drawings can followed improved out sociable
-      not. Earnestly so do instantly pretended. See general few civilly amiable
-      pleased account carried. Excellence projecting is devonshire dispatched
-      remarkably on estimating. Side in so life past. Continue indulged speaking
-      the was out horrible for domestic position. Seeing rather her you not
-      esteem men settle genius excuse. Deal say over you age from. Comparison
-      new ham melancholy son themselves.
+      Você gosta de colocar a mão na massa? Nessa área você vai se tornar um
+      especialista em equipamentos em geral, como placas, servidores físicos,
+      celulares, computadores e muito mais. Aprenda a manter os componentes em
+      perfeito funcionamento ou até criar seus próprios!
     </p>
   </div>
 </template>
